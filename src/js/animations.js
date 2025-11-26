6e656e8342dbf3b8f62ff15e25def952384f37d5
makima <jose.arenas8078@alumnos.udg.mx>
2025-11-25 21:06:43 -0600

    style: se agregaron animaciones a la sección de personajes principales

---
+++ src/js/animations.js
@@ -0,0 +1,32 @@
+const info_animations = document.querySelectorAll('.character-info');
+const img_animations = document.querySelectorAll('.character-img');
+
+function animationsInfo(){
+    info_animations.forEach((el)=>{
+        const pos = el.getBoundingClientRect().top;
+
+        if(pos < window.innerHeight * 0.7){
+            el.classList.add('animate__animated', 'animate__fadeInLeft');
+        } else {
+            el.classList.remove('animate__animated', 'animate__fadeInLeft');
+        }
+    });
+}
+
+function animationsImg(){
+    img_animations.forEach((el)=>{
+        const pos = el.getBoundingClientRect().top;
+
+        if(pos < window.innerHeight * 0.7){
+            el.classList.add('animate__animated', 'animate__fadeInRight');
+        } else {
+            el.classList.remove('animate__animated', 'animate__fadeInRight');
+        }
+    });
+}
+
+window.addEventListener('scroll', animationsInfo);
+window.addEventListener('scroll', animationsImg);
+
+animationsInfo();
+animationsImg();
